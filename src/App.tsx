39a3eeac82a6a00eaa26b607a8d986ba1a0769a1
Wyat Soule <wsoule679@gmail.com>
2022-11-15 16:41:53 -0500

    set up project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { Count } from './items/cookie';
 
-function App() {
+function App() : JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Count />
     </div>
   );
 }
---
+++ src/items/cookie.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+export const Count : FC = () => {
+  return (
+    <p>Hello</p>
+  );
+};
